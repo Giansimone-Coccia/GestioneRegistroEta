1e64c2f56637198b904c77e77866f7253614e55d
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-12-02 18:19:45 +0100

    Aggiunto Javadoc

--- src/Gestione/NomeEta.java
+++ src/Gestione/NomeEta.java
@@ -1,33 +1,55 @@
 package Gestione;
 
-//Definizione classe
+/**
+ * Definizione classe NomeEta
+ * @author gians_ji5genm
+ *
+ */
 public class NomeEta {
 	private String nome;
 	private int eta;
 	
-	//Costruttore
+	/**
+	 * Costruttore
+	 * @param nome Nome persona
+	 * @param eta Età persona
+	 */
 	public NomeEta(String nome, int eta) {
 		this.nome = nome;
 		this.eta = eta;
 	}
 	
-	//Vari metodi get e set
+	/**
+	 * Metodo per ottenere il nome
+	 * @return nome
+	 */
 	public String getNome() {
 		return nome;
 	}
-	
+	/**
+	 * Metodo per ottenere l'età
+	 * @return eta
+	 */
 	public int getEta() {
 		return eta;
 	}
-	
+	/**
+	 * Metodo per settare il nome
+	 * @param nome persona
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+	/**
+	 * Metodo per settare l'età
+	 * @param eta persona
+	 */
 	public void setEta(int eta) {
 		this.eta = eta;
 	}
-	
+	/**
+	 * Incrementa l'età
+	 */
 	public void incrementaEta() {
 		++eta;
 	}
--- src/Gestione/RegistroEtaCompleto.java
+++ src/Gestione/RegistroEtaCompleto.java
@@ -1,20 +1,30 @@
 package Gestione;
 import java.util.Vector;
 
-//Definizione classe
+/**
+ * Definizione classe RegistroEtaCompleto in cui un vettore
+ * ospita tutte le persone
+ * @author gians_ji5genm
+ *
+ */
 public class RegistroEtaCompleto {
 	Vector<NomeEta> noEta = new Vector<NomeEta>();
 	public RegistroEtaCompleto() {
 		//new Vector<NomeEta>();
 	}
 	
-	//Metodo per aggiungere un nuovo elemento
+	/**
+	 * Metodo per aggiungere un nuovo elemento
+	 * @param elem di tipo NomeEta
+	 */
 	public void aggiungeElemento(NomeEta elem) {
 		noEta.add(elem);
 	}
 	
-	/*Metodo per la ricerca di un elemento tramite il nome e
+	/**
+	 * Metodo per la ricerca di un elemento tramite il nome e
 	 * conseguente incremento dell'età
+	 * @param nome
 	 */
 	public void cercaElemento(String nome) {
 		for(int i = 0; i < noEta.size(); i++) {
@@ -25,7 +35,9 @@ public class RegistroEtaCompleto {
 		System.out.println("Hai incrementato l'età di "+ nome);
 	}
 	
-	//@override toString
+	/**
+	 * @override toString
+	 */
 	public String toString() {
 		for(int i = 0; i < noEta.size(); i++) {
 			return noEta.get(i).getNome() + " "+ noEta.get(i).getEta();
